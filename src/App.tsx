f069c4df6e05579401238394e334f91697a869bd
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-16 12:32:58 +0100

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Sidebar from './components/sidebar';
 
+interface Props {}
+interface State {
+  isSidebarOpen: boolean;
+}
 
 export default class App extends Component<Props, State> {
+
+  state: State = {
+    isSidebarOpen: false
+  }
+
+  toggleSlider = () => {
+    this.setState({ isSidebarOpen: !this.state.isSidebarOpen})
+  }
 
   render() {
     return (
       <>
-        <Header></Header>
-        
+        <Header onMenuClick={this.toggleSlider}/>
+        <Sidebar isOpen={this.state.isSidebarOpen}/>
       </>
     );
   }
