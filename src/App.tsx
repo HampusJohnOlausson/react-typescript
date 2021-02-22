f2faa44d505bf0d0842d872c440041acf0810045
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-02-22 09:14:29 +0100

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
 import React, { Component, CSSProperties } from 'react';
-import Input from './components/Input'
 
-interface Props{}
-interface State{
-  searchValue: string;
+export default class App extends React.Component{
 
-}
+ 
 
-export default class App extends React.Component<Props, State>{
-
-  state: State = {
-    searchValue: ''
-  }
-
-  handleNewSearchValue = (value: string) => {
-    this.setState({ searchValue: value})
-  }
 
   render() {
     
     return (
       <React.Fragment>
-        <Input
-        value={this.state.searchValue} 
-        onChange={this.handleNewSearchValue}
-        />
+       
       </React.Fragment>
     );
   }
