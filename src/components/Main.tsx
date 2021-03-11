6ed26e22ff6b1d3a652142cea0ced03596cf7de8
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-11 20:56:50 +0100

    useState hooks exercise

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Main = () => {
+
+    const [items, setItems] = useState([]);
+    
+    const addItem = () => {
+        setItems([...items, {
+            id: items.length,
+            value: Math.floor(Math.random() * 10 ) + 1
+        } ])
+    }
+
     return (
         <div>
-            
+            <div>
+                <button onClick={addItem}>add a number</button>
+            </div>
+            <ul>
+                {
+                    items.map(item => (
+                        <li key={item.id}>{item.value}</li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
